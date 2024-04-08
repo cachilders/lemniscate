4871128b87691e1616177ba8b3f58fc8f62a25e6
Casey Childers <cachilders@gmail.com>
2024-04-08 01:12:59 +0100

    [feat] it jumps and toggles

--- lemniscate.lua
+++ lemniscate.lua
@@ -1,5 +1,5 @@
 -- lemniscate
---
+-- 
 
 local util = require('util')
 
@@ -8,13 +8,24 @@ local position = 1
 local program = 1
 local program_length = 10
 local tape_length = program_length * 4
-local source_amp = 0.5
-local play_amp = 0.9
-local rec_amp = 0.9
+local source_amp = 1.0
+local play_amp = 1.0
+local rec_amp = 1.0
 local preserve_amp = 0.7
+local shift = false
+local playing = 0
+local recording = 0
 
 local function _refresh_position(i, pos)
   position = math.floor(pos)
+end
+
+local function _set_head_position()
+  for i = 1, 2 do
+    softcut.position(i, position)
+  end
+
+  softcut.voice_sync(2, 1, 0)
 end
 
 local function init_softcut()
@@ -39,21 +50,37 @@ local function get_program_offset()
   return (program - 1) * program_length
 end
 
-local function play()
+local function toggle_play()
+  playing = util.wrap(playing + 1, 0, 1)
+
   for i = 1, 2 do
-    softcut.play(i, 1)
+    softcut.play(i, playing)
   end
 
   softcut.voice_sync(2, 1, 0)
 end
 
-local function record()
+local function toggle_record()
+  recording = util.wrap(recording + 1, 0, 1)
+
   for i = 1, 2 do
-    softcut.rec(i, 1)
+    softcut.rec(i, recording)
   end
 
   softcut.voice_sync(2, 1, 0)
 end
+
+local function stop_all()
+  for i = 1, 2 do
+    softcut.play(i, 0)
+    softcut.rec(i, 0)
+  end
+
+  position = 1
+  program = 1
+  _set_head_position()
+end
+
 
 local function program_select(n)
   local segment_position = position - get_program_offset()
@@ -65,7 +92,7 @@ local function program_select(n)
   end
 
   position = segment_position + get_program_offset()
-  set_head_position()
+  _set_head_position()
 end
 
 local function refresh_program()
@@ -77,14 +104,6 @@ local function refresh_program()
   end
 end
 
-local function set_head_position()
-  for i = 1, 2 do
-    softcut.loop_position(i, position)
-  end
-
-  softcut.voice_sync(2, 1, 0)
-end
-
 local function set_tape_length(d)
   program_length = util.clamp(program_length + d, 1, MAX_PROGRAM_LENGTH)
   for i = 1, 2 do
@@ -92,21 +111,31 @@ local function set_tape_length(d)
   end
 
   position = util.clamp(position, 1, program_length)
-  set_head_position()  
+  _set_head_position()  
 end
 
 local function temp_render_text()
   screen.move(64, 12)
   screen.text_center('PROGRAM '..program)
   screen.move(64, 22)
-  screen.text_center('PLAY POSITION '..position)
+  screen.text_center('POSITION '..position)
+  screen.move(64, 32)
+  if playing == 0 and recording == 0 then
+    screen.text_center('PAUSED')
+  elseif playing == 0 then
+    screen.text_center('PLAY PAUSED / REC ACTIVE')
+  elseif recording == 0 then
+    screen.text_center('PLAY ACTIVE / REC PAUSED')
+  else
+    screen.text_center('PLAY ACTIVE / REC ACTIVE')
+  end
 end
   
 
 function init()
   init_softcut()
-  play()
-  record()
+  toggle_play()
+  toggle_record()
   redraw()
 end
 
@@ -115,8 +144,19 @@ function enc(e, d)
 end
 
 function key(k, z)
-  -- change program
-  -- play/pause/rec/stop
+  if k == 1 and z == 1 then
+    shift = true
+  elseif k == 1 and z == 0 then
+    shift = false
+  elseif k == 2 and z == 0 and not shift then
+    toggle_play()
+  elseif k== 2 and z == 0 and shift then
+    toggle_record()
+  elseif k == 3 and z == 0 and not shift then
+    program_select()
+  elseif k == 3 and z == 0 and shift then
+    stop_all()
+  end
 end
 
 function redraw()
